0536352b860966f43960da05d7d96a9f5456254f
anhvu <anhvu@DESKTOP-SDCRVG7>
2022-12-29 20:44:54 +0700

    filter user

--- eShopAdminApp/Controllers/LoginController.cs
+++ eShopAdminApp/Controllers/LoginController.cs
@@ -35,6 +35,11 @@ namespace eShopAdminApp.Controllers
                 return View(ModelState);
 
             var result = await _userApiClient.Authenticate(request);
+            if (result.ResultObj == null)
+            {
+                ModelState.AddModelError("", result.Message);
+                return View();
+            }
             var userPrincipal = this.ValidateToken(result.ResultObj);
             var authProperties = new AuthenticationProperties
             {
